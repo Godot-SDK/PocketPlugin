cab4fe40f2d6cfa2d31c44d656a9ebb4e28e0c0a
2439905184 <suchenglong018@126.com>
2021-04-03 14:34:22 +0800

    现在可以正常被编译使用了
    现在可以正常被编译使用了

--- src/main/java/com/hh128/StrFromTest.java
+++ src/main/java/com/hh128/StrFromTest.java
@@ -1,9 +1,13 @@
 package com.hh128;
 
 
+import android.app.Activity;
+import android.view.View;
+import android.widget.FrameLayout;
+
 import androidx.annotation.NonNull;
 
-import com.example.mystrinng.MyString;
+//import com.example.mystrinng.MyString;
 
 import org.godotengine.godot.Godot;
 import org.godotengine.godot.plugin.GodotPlugin;
@@ -12,11 +16,16 @@ import java.util.Arrays;
 import java.util.List;
 public class StrFromTest extends GodotPlugin
 {
-
+    public FrameLayout layout;
     public StrFromTest(Godot godot) {
         super(godot);
     }
-
+    @Override
+    public View onMainCreate(Activity activity)
+    {
+        this.layout=new FrameLayout(activity);
+        return this.layout;
+    }
     @NonNull
     @Override
     public List<String> getPluginMethods()
@@ -26,8 +35,8 @@ public class StrFromTest extends GodotPlugin
     //从依赖aar获取字符串
     public String getStr()
     {
-       // return "ADS";
-       return MyString.mystring;
+       return "ADS";
+       //return MyString.mystring;
     }
     @NonNull
     @Override
