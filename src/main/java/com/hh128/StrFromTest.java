9aa1cccd8b17a720c1b871cef38bb9fcbc84f1f2
2439905184 <suchenglong018@126.com>
2021-04-04 21:34:42 +0800

    备份 对接口袋工厂sdk
    备份 对接口袋工厂sdk

--- src/main/java/com/hh128/StrFromTest.java
+++ src/main/java/com/hh128/StrFromTest.java
@@ -4,21 +4,32 @@ package com.hh128;
 import android.app.Activity;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.zh.pocket.ads.LEADSdk;
+import com.zh.pocket.ads.banner.BannerAD;
+import com.zh.pocket.ads.interstitial.InterstitialAD;
+import com.zh.pocket.ads.interstitial.InterstitialADListener;
+import com.zh.pocket.base.LESdk;
+import com.zh.pocket.base.bean.LEError;
+import com.zh.pocket.base.common.config.LEConfig;
 
 import androidx.annotation.NonNull;
-
-//import com.example.mystrinng.MyString;
 
 import org.godotengine.godot.Godot;
 import org.godotengine.godot.plugin.GodotPlugin;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 public class StrFromTest extends GodotPlugin
 {
     public FrameLayout layout;
-    public StrFromTest(Godot godot) {
+    public Activity activity;
+    public StrFromTest(Godot godot)
+    {
         super(godot);
+        activity=getActivity();
     }
     @Override
     public View onMainCreate(Activity activity)
@@ -30,13 +41,64 @@ public class StrFromTest extends GodotPlugin
     @Override
     public List<String> getPluginMethods()
     {
-        return Arrays.asList(new String[]{"getStr"});
+        //return Arrays.asList(new String[]{"getStr"});
+        return new ArrayList<String>()
+        {
+            {
+                add("getStr");
+                add("init");
+                add("showAd");
+            }
+        };
     }
     //从依赖aar获取字符串
     public String getStr()
     {
        return "ADS";
-       //return MyString.mystring;
+     //  return MyString.mystring;
+    }
+    //先用测试id
+    public void init()
+    {
+      //  LEConfig.setEnableLogger(true);
+    }
+    //测试视频广告
+    public void showAd()
+    {
+        InterstitialAD ad= new InterstitialAD(getActivity(), new InterstitialADListener() {
+            @Override
+            public void onADReceive() {
+
+            }
+            @Override
+            public void onADExposure() {
+            }
+            @Override
+            public void onADClicked()
+            {
+            }
+            @Override
+            public void onADClosed()
+            {
+            }
+            @Override
+            public void onSuccess()
+            {
+
+            }
+            @Override
+            public void onFailed(LEError leError)
+            {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run()
+                    {
+                        Toast.makeText(getActivity(),"失败",Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+        ad.show();
     }
     @NonNull
     @Override
---
+++ src/main/java/com/xiaofengyu/travel/MyApp.java
@@ -0,0 +1,27 @@
+package com.xiaofengyu.travel;
+
+import android.app.Application;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.zh.pocket.ads.LEADSdk;
+import com.zh.pocket.base.LESdk;
+import com.zh.pocket.base.common.config.LEConfig;
+import com.zh.pocket.base.imageloader.GlideImageLoader;
+
+public class MyApp extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        Log.i("MyApp","初始化sdk");
+        //LEConfig.setEnableLogger(true);
+        LESdk.initSDK("test", "10006");
+        LESdk.setImageLoader(new GlideImageLoader());
+        LEADSdk.initSDK(this);
+        Log.i("MyApp","初始化执行完毕");
+        //Toast.makeText(this, "初始化完毕", Toast.LENGTH_SHORT).show();
+    }
+
+}
