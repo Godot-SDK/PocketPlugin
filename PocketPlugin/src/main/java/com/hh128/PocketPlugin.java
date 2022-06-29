4af9d225fb426029a8415b5f37acd4817afb6df0
2439905184 <suchenglong018@126.com>
2022-06-29 16:52:17 +0800

    🐞 fix: 大更新
    添加播放前请求权限，fix 广告播放bug

--- PocketPlugin/src/main/java/com/hh128/PocketPlugin.java
+++ PocketPlugin/src/main/java/com/hh128/PocketPlugin.java
@@ -1,38 +1,47 @@
 package com.hh128;
 
-
+import android.Manifest;
 import android.app.Activity;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.FragmentActivity;
 
 import com.zh.pocket.ads.banner.BannerAD;
 import com.zh.pocket.ads.banner.BannerADListener;
 import com.zh.pocket.ads.fullscreen_video.FullscreenVideoAD;
 import com.zh.pocket.ads.fullscreen_video.FullscreenVideoADListener;
-
 import com.zh.pocket.ads.interstitial.InterstitialAD;
 import com.zh.pocket.ads.interstitial.InterstitialADListener;
 import com.zh.pocket.ads.nativ.NativeAD;
 import com.zh.pocket.ads.nativ.NativeADListener;
-import com.zh.pocket.ads.reward_video.RewardVideoAD;
 import com.zh.pocket.ads.reward_video.RewardVideoADListener;
 import com.zh.pocket.error.ADError;
-
-import androidx.annotation.NonNull;
 
 import org.godotengine.godot.Godot;
 import org.godotengine.godot.plugin.GodotPlugin;
 import org.godotengine.godot.plugin.SignalInfo;
 import org.godotengine.godot.plugin.UsedByGodot;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
-
 public class PocketPlugin extends GodotPlugin
 {
+
     //定义信号名字 信号链接是在godot链接的
     public SignalInfo adReady = new SignalInfo("adReady");
 
@@ -59,17 +68,107 @@ public class PocketPlugin extends GodotPlugin
     public SignalInfo NativeAdFailed = new SignalInfo("NativeAdFailed");
 
     public FrameLayout layout;
-    public Activity activity;
+    public FragmentActivity activity;
     public String Tag;
     //插件自己的Godot 暂不使用
     public Godot plugin_godot;
+    private  RewardVideoAD VideoAd;
+    private PluginRewardVideoAdListener rewardVideoAdListener;
+
+    String[] permissions = new String[]{Manifest.permission.REQUEST_INSTALL_PACKAGES,Manifest.permission.EXPAND_STATUS_BAR,Manifest.permission.SYSTEM_ALERT_WINDOW,Manifest.permission.ACCESS_WIFI_STATE,Manifest.permission.ACCESS_NETWORK_STATE,Manifest.permission.READ_PHONE_STATE,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.LOCATION_HARDWARE,Manifest.permission.GET_TASKS,Manifest.permission.WAKE_LOCK,Manifest.permission.INTERNET,Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    List<String> mPermissionList = new ArrayList<>();
+    private static final int MY_PERMISSION_REQUEST_CODE = 10000;
+
+    // private ImageView welcomeImg = null;
+    private static final int PERMISSION_REQUEST = 1;
+    /**
+     * 响应授权
+     * 这里不管用户是否拒绝，都进入首页，不再重复申请权限
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void checkPermission() {
+        mPermissionList.clear();
+        //判断哪些权限未授予
+        for (int i = 0; i < permissions.length; i++) {
+            if (activity.checkSelfPermission( permissions[i]) != PackageManager.PERMISSION_GRANTED) {
+                mPermissionList.add(permissions[i]);
+            }
+        }
+        /**
+         * 判断是否为空
+         */
+        if (mPermissionList.isEmpty()) {//未授予的权限为空，表示都授予了
+        } else {//请求权限方法
+            String[] permissions = mPermissionList.toArray(new String[mPermissionList.size()]);//将List转为数组
+
+            activity.requestPermissions(permissions, PERMISSION_REQUEST);
+        }
+    }
+
+    @Override
+    public void onMainRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        super.onMainRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == MY_PERMISSION_REQUEST_CODE) {
+            boolean isAllGranted = true;
+
+            // 判断是否所有的权限都已经授予了
+            for (int grant : grantResults) {
+                if (grant != PackageManager.PERMISSION_GRANTED) {
+                    isAllGranted = false;
+                    break;
+                }
+            }
+
+            if (isAllGranted) {
+                // 如果所有的权限都授予了, 则执行备份代码
+                Toast.makeText(getActivity(),"已经授予全部权限",Toast.LENGTH_SHORT).show();
+
+            } else {
+                // 弹出对话框告诉用户需要权限的原因, 并引导用户去应用权限管理中手动打开权限按钮
+                openAppDetails();
+            }
+        }
+
+    }
+
+
+
+    /**
+     * 打开 APP 的详情设置
+     */
+    private void openAppDetails() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("本游戏需要访问 “网络” 和 “外部存储器”，请到 “应用信息 -> 权限” 中授予！");
+        builder.setPositiveButton("去手动授权", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                intent.setData(Uri.parse("package:" + getActivity().getPackageName()));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                getActivity().startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("取消", null);
+        builder.show();
+    }
+
+    private static Godot godot;
     public PocketPlugin(Godot godot)
     {
         super(godot);
-        activity=getActivity();
+        this.godot=godot;
+        activity=godot.getActivity();
         //获取到godot的根布局 并动态添加线性布局用于当做banner广告容器
         //layout = getGodot().getFragmentManager().getFragment();
         Tag= PocketPlugin.class.toString();
+
+        rewardVideoAdListener=new PluginRewardVideoAdListener();
     }
     @NonNull
     @Override
@@ -114,12 +213,27 @@ public class PocketPlugin extends GodotPlugin
     /**
      *展示激励视频广告
      */
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @UsedByGodot
     public void showRewardVideoAd(String id){
-        RewardVideoAD VideoAd = new RewardVideoAD(getActivity(), id);
-        VideoAd.setRewardVideoADListener(new PluginRewardVideoAdListener());
-        VideoAd.loadAD();
-    }
+
+       // id="55387";
+        checkPermission();
+                VideoAd = new RewardVideoAD(getGodot().getActivity(), id);
+               if (VideoAd!=null){
+
+                VideoAd.setRewardVideoADListener(rewardVideoAdListener);
+                VideoAd.loadAD(true);
+
+                }
+                else{
+                   VideoAd.destroy();
+                }
+                //VideoAd.showAD();
+
+
+        }
     //已完成
     /*展示插屏广告*/ //官方测试id6（需配合官方包名）
     @UsedByGodot
@@ -171,6 +285,7 @@ public class PocketPlugin extends GodotPlugin
         NativeAD nativeAD = new NativeAD(getActivity(),id,vg);
         nativeAD.setNativeADListener(new PluginNativeAdListener());
         nativeAD.loadAD();
+        nativeAD.show();
 
     }
     class PluginBannerLinstener implements BannerADListener
@@ -256,25 +371,8 @@ public class PocketPlugin extends GodotPlugin
     //奖励广告回调
     class PluginRewardVideoAdListener implements RewardVideoADListener
     {
-        //展示广告
-        @Override
-        public void onADShow()
-        {
-            Log.i("RewardVideo","奖励视频被展示");
-        }
 
-        @Override
-        public void onADExposure()
-        {
-            Log.i("RewardVideo","奖励视频被曝光");
-        }
 
-        @Override
-        public void onVideoCached()
-        {
-            Log.i("RewardVideo","奖励视频被缓存");
-        }
-        //当成功获得奖励
         @Override
         public void onReward() {
             //发送信号,允许获得奖励
@@ -296,9 +394,12 @@ public class PocketPlugin extends GodotPlugin
         @Override
         public void onADClosed()
         {
+
             Log.d("RewardVideo","奖励视频被关闭");
             emitSignal(RewardClosed.getName());
+            VideoAd.destroy();
         }
+
 
         @Override
         public void onFailed(ADError adError)
@@ -313,6 +414,24 @@ public class PocketPlugin extends GodotPlugin
         {
             Log.d("RewardVideo","激励视频加载成功");
         }
+
+        @Override
+        public void onVideoCached() {
+            Log.i("RewardVideo","广告被缓存了");
+
+            VideoAd.showAD();
+        }
+
+        @Override
+        public void onADShow() {
+            Log.i("RewardVideo","广告展示中");
+        }
+
+        @Override
+        public void onADExposure() {
+            Log.i("RewardVideo","广告已曝光");
+        }
+
         @Override
         public void onSkippedVideo()
         {
---
+++ PocketPlugin/src/main/java/com/hh128/RewardVideoAD.java
@@ -0,0 +1,20 @@
+package com.hh128;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+
+import android.app.Activity;
+
+public class RewardVideoAD extends com.zh.pocket.ads.reward_video.RewardVideoAD {
+
+    public RewardVideoAD(Activity activity, String s) {
+        super(activity, s);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
